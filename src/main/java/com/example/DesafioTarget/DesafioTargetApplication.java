bd303abd742a61894758d2c540fa940e77ca5c99
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-05-11 09:18:27 -0300

    criação do repositório/spring

---
+++ src/main/java/com/example/DesafioTarget/DesafioTargetApplication.java
@@ -0,0 +1,13 @@
+package com.example.DesafioTarget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTargetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTargetApplication.class, args);
+	}
+
+}
